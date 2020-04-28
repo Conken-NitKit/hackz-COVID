874e5ecef7d02134849a0fe108f3055e4f65102e
kubo-hide-kun <hide44041@gmail.com>
2020-04-28 17:23:12 +0900

    style: インデントの変更とStyledComponentsの位置の修正

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -7,64 +7,64 @@ import html from 'rehype-stringify';
 import styled from 'styled-components';
 import COLOR from '../../styles/color';
 
+const EditorSet = (props) => {
+    const [content, setContent] = useState("");
+    const { contents } =
+        unified()
+        .use(markdown)
+        .use(slug)
+        .use(remark2rehype)
+        .use(html)
+        .processSync(content)
+
+    const handleChange = (e) => {
+        setContent(e.target.value);
+        console.log(content);
+        console.log(contents);
+    }
+    return <Wrapper>
+        <Editor value={content} onChange={handleChange} />
+        <Viewer mode={props.mode} dangerouslySetInnerHTML={{ __html: contents}} />
+    </Wrapper>;
+}
+
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+    width: 100vw;
+    height: 100vh;
 `;
 
 const Editor = styled.textarea`
-  display: inline-block;
-  width: 43%;
-  height: 98%;
-  margin: 1%;
-  resize: none;
-  border: none;
-  outline: none;
-  font: 1.5rem bold;
-  background: ${(props)=>COLOR.BACKGROUND[props.mode]};
+    display: inline-block;
+    width: 43%;
+    height: 98%;
+    margin: 1%;
+    resize: none;
+    border: none;
+    outline: none;
+    font: 1.5rem bold;
+    background: ${(props)=>COLOR.BACKGROUND[props.mode]};
 `;
 
 const Viewer = styled.div`
-  display: inline-block;
-  width: 45%;
-  height: 100%;
-  padding: 0.5%;
-  overflow: scroll;
-  text-align: left;
-  background: ${(props)=>COLOR.BACKGROUND[props.mode]};
-  color: ${(props)=>COLOR.TEXT[props.mode]};
-  & h1,h2,h3,h4,h5 {
-    color: ${(props)=>COLOR.ACCENT[props.mode]};
-  }
-  & strong {
-    color: ${(props)=>COLOR.ACCENT[props.mode]};
-  }
-  & hr {
-    height: 1.5px;
-    background-color: ${(props)=>COLOR.BORDER[props.mode]};
-    border: none;
-  }
+    display: inline-block;
+    width: 45%;
+    height: 100%;
+    padding: 0.5%;
+    overflow: scroll;
+    text-align: left;
+    background: ${(props)=>COLOR.BACKGROUND[props.mode]};
+    color: ${(props)=>COLOR.TEXT[props.mode]};
+    & h1,h2,h3,h4,h5 {
+        color: ${(props)=>COLOR.ACCENT[props.mode]};
+    }
+    & strong {
+        color: ${(props)=>COLOR.ACCENT[props.mode]};
+    }
+    & hr {
+        height: 1.5px;
+        background-color: ${(props)=>COLOR.BORDER[props.mode]};
+        border: none;
+    }
 `;
-
-const EditorSet = (props) => {
-  const [content, setContent] = useState("");
-  const { contents } =
-    unified()
-    .use(markdown)
-    .use(slug)
-    .use(remark2rehype)
-    .use(html)
-    .processSync(content)
-
-  const handleChange = (e) => {
-    setContent(e.target.value);
-    console.log(content);
-    console.log(contents);
-  }
-  return <Wrapper>
-    <Editor value={content} onChange={handleChange} />
-    <Viewer mode={props.mode} dangerouslySetInnerHTML={{ __html: contents}} />
-  </Wrapper>;
-}
 
 export default EditorSet;
