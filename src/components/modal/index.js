394e473fd657668fbea8142dc9b11175883626c9
Marumo0531 <marumo31dev@gmail.com>
2020-04-29 20:58:57 +0900

    Add: modal/index.jsの追加(最新verで)

---
+++ src/components/modal/index.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import COLOR from './styles/color';
+
+//############################################################
+//  メインメソッド
+//############################################################
+const Modal = () => {
+
+}
+
+export default Modal;
