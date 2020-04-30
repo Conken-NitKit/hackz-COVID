a45f55cbebb0a9a7da80f78cb24e1ac2dbb32b02
Marumo0531 <marumo31dev@gmail.com>
2020-04-30 21:53:15 +0900

    Update: ヘッダーとタイトルを実装

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import COLOR from './styles/color';
+import COLOR from '../../styles/color';
 
 //############################################################
 //コンポーネント
@@ -11,15 +11,48 @@ const Main = styled.div`
     position:relative;
     width: 100vw;
     height: 9vh;
+
+    background: ${(props) => (props.isHome && props.mode === "LIGHTMODE" ? "#202020" : COLOR.MAIN[props.mode])};
+`;
+
+//タイトル 
+const Title = styled.div`
+    position:absolute;
+    width: 150px;
+    height: 40px;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 37px;
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    color: #FFFFFF;
 `;
 
 //############################################################
 //メインメソッド
 //############################################################
-const Header = () => {
-    <Main>
-        
-    </Main>
+const Header = (props) => {
+    const {
+        mode, // モード(LIGHTMODE or DARKMODE)
+        isHome //ホーム画面が表示されているか否か
+    } = props;
+
+    return (
+        <Main mode={mode} isHome={isHome}>
+            <Title>タイトル</Title>
+        </Main>
+    );
 }
 
 export default Header;
