f3211bbe14b13304fa9ad177463c26da8daf8662
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-04-30 20:00:26 +0900

    Add: 残りのポスト処理を実装しました。 #18

--- src/functions/main.js
+++ src/functions/main.js
@@ -30,3 +30,44 @@ export const signIn = async(email, passward) => {
   }
   return await axios.post(baseUrl + 'users/sign_in', postData)
 }
+
+export const initMeeting = async(title, discription, ownerId) => {
+  const postData = {
+    'title': title,
+    'discription': discription,
+    'owner_id': ownerId
+  }
+  return await axios.post(baseUrl + 'meetings/init', postData)
+}
+
+export const addUser = async(meetingId, userId) => {
+  const postData = {
+    'meeting_id': meetingId,
+    'userId': userId
+  }
+  return await axios.post(baseUrl + 'meetings/add_record', postData)
+}
+
+export const addRecord = async(meetingId, userId, recordData) => {
+  const postData = {
+    'meeting_id': meetingId,
+    'user_id': userId,
+    'record_data': recordData
+  }
+  return await axios.post(baseUrl + 'meetings/add_record', postData)
+}
+
+export const addBlackList = async(meetingId, blackWords) => {
+  const postData = {
+    'meeting_list': meetingId,
+    'black_words': blackWords
+  }
+  return await axios.post(baseUrl + 'meetings/add_black_list', postData)
+}
+
+export const finishMeeting = async(meetingId) => {
+  const postData = {
+    'meeting_id': meetingId
+  }
+  return await axios.post(baseUrl + 'meetings/finish', postData)
+}
